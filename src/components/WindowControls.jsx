72f48e18d918d8cd9085253b8bd73d69181fad41
vansh-09 <vanshkjain09@gmail.com>
2025-11-24 01:42:14 +0530

    implement a hoc for managing windows and added first window - terminal

---
+++ src/components/WindowControls.jsx
@@ -0,0 +1,11 @@
+const WindowControls = ({ target }) => {
+  const { closeWindow } = useWindowStore();
+  return <div id="window-controls">
+    <div className="close" onClick={() => closeWindow(target)} />
+    <div className="minimize" onClick={closeWindow} />
+    <div className="maximize" onClick={closeWindow} />
+  </div>;
+};
+
+export default WindowControls;
+WindowControls;
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Navbar from '#components/Navbar.jsx';
 import Welcome from '#components/Welcome.jsx';
 import Dock from '#components/Dock.jsx';  
 import { dockApps } from '#constants';  
+import WindowControls from '#components/WindowControls.jsx';
 
 
-export { Navbar, Welcome, Dock };
+export { Navbar, Welcome, Dock, WindowControls };
